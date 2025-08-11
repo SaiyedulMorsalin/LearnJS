1c67dedea85eabb1988c93f38cc5eecaf7176a6a
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2025-08-11 17:59:27 +0000

    create multiple variable creation erros

--- basic_js/js.js
+++ basic_js/js.js
@@ -1,2 +1,32 @@
 let a = 10;
 console.log(a)
+let x,y,z;
+x =100;
+y=100;
+z=x+y;
+console.log("Summation of x,y,z")
+console.log(z)
+
+a=10;b=30;c=20; d=a+b+c; //inline statement of javascript
+console.log("############")
+console.log(d)
+console.log("###########")
+
+//javaScript keywords
+
+// Keyword	Description
+// var	Declares a variable
+// let	Declares a block variable
+// const	Declares a block constant
+// if	Marks a block of statements to be executed on a condition
+// switch	Marks a block of statements to be executed in different cases
+// for	Marks a block of statements to be executed in a loop
+// function	Declares a function
+// return	Exits a function
+
+var x;
+var y;
+x = 10;
+y = 20;
+let z = x+y;
+console.log(z)
